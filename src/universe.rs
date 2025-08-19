5dc91dc4b71ff902a15f680e14dc30ce7940e5ad
timdbg <190715356+timdbg@users.noreply.github.com>
2025-08-19 23:54:43 +0200

    fix: wording

--- src/universe.rs
+++ src/universe.rs
@@ -143,7 +143,7 @@ impl Universe {
         // Recompute star values that depend on planet (tidal and magnetic torque).
         star.refresh_tidal_frequency(planet);
 
-        // Compute the enabled effects (magnetism, stellar tides, planet tides)
+        // Compute the enabled effects dependent on a planet (magnetism, planet tides)
         star.update_tidal_torque(self.central_body.tides.tidal_torque(star, planet));
         star.update_magnetic_torque(self.central_body.magnetism.magnetic_torque(planet, star)); // Requires wind torque to be calculated first.
 
